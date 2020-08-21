a70c989779a5b7c291c0ae1099a20dcedc9005c8
Glitch (flask-app-starter) <none>
2020-08-21 09:30:55 +0000

    🐲🏙 Checkpoint ./show_all.html:21382522/1760 ./app.py:21382522/900 ./templates/show_all.html:21382522/880

--- app.py
+++ app.py
@@ -45,36 +45,11 @@ def home():
   else: 
     return render_template("index.html")
 
-# @app.route("/<usr>/<email>")
-# def pri(usr,email):
-#     return f'<html><h1>{usr}</h1></html>'
+@app.route("/show_all")
+def show():
+  return render_template("show_all.html", values=User.query.all())
 
 
-
-
-# class Interviewer(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     Interviewer = db.Column(db.String(50))
-#     Date = db.Column(db.Date)		
-#     slot=db.Column(db.String(50))
-
-
-
-@app.route('/<name>/<location>')
-def index(name, location):
-    user = User(name=name, location=location)
-    db.session.add(user)
-    db.session.commit()
-
-    return '<h1>Added New User!</h1>'
-
-@app.route('/<name>')
-def get_user(name):
-    user = User.query.filter_by(name=name).first()
-
-    return  '<h1>Added New Use3!</h1>'
-  
-  
 if __name__ == '__main__':
     db.create_all()
     app.secret_key = os.urandom(24)
