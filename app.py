4aaa4d73e9f6e64aae0a2a44f2957632f7320e30
Glitch (flask-app-starter) <none>
2020-08-22 15:10:51 +0000

    🐿🎨 Checkpoint ./app.py:21382522/877

--- app.py
+++ app.py
@@ -16,7 +16,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///.data/db.sqlite3'
 
 
 
-app.config[''] =
+app.config['DEBUG'] = True
+app.config['TESTING'] = False
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME '] = 'vandanrkt@gmail.com'
+app.config['MAIL_PASSWORD '] = ''
+app.config[]
+
 
 
 
